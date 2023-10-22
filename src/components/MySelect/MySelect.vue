d297805b11b4c6d2423ecc4456fe7207effbae7e
mykhail.svekolnykov <Mykhail.Svekolnykov.-ND@disney.com>
2023-10-22 21:46:40 +0300

    Add custom select

---
+++ src/components/MySelect/MySelect.vue
@@ -0,0 +1,55 @@
+<script lang="ts" setup="">
+import { ref } from "vue";
+
+const props = defineProps({
+  options: Array,
+  handleFilterChange: Function,
+  label: String,
+  value: String,
+});
+
+const selectedValue = ref(props.value);
+const emit = defineEmits(["input"]);
+
+const handleChange = (event) => {
+  selectedValue.value = event.target.value;
+  emit("input", selectedValue);
+  props.handleFilterChange();
+};
+</script>
+
+<template>
+  <div class="select-wrapper">
+    <label>{{ label }}</label>
+    <select v-model="selectedValue" @change="handleChange" class="select-style">
+      <option
+        v-for="option in options"
+        :value="option.value"
+        :key="option.value"
+        class="option-style"
+      >
+        {{ option.label }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "../../styles/_variables.scss";
+.select-wrapper {
+  display: flex;
+  flex-direction: column;
+  margin-right: 20px;
+}
+.select-style {
+  padding: 8px;
+  font-size: 16px;
+  border: 1px solid $border-grey;
+  border-radius: 4px;
+}
+
+.option-style {
+  padding: 8px;
+  font-size: 16px;
+}
+</style>
