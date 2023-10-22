35d1e91e088d985abcc143a10f2ae40f429bed2b
mykhail.svekolnykov <Mykhail.Svekolnykov.-ND@disney.com>
2023-10-22 21:46:59 +0300

    Add mock user info

--- src/store/authModule.ts
+++ src/store/authModule.ts
@@ -26,7 +26,31 @@ const authModule: Module<AuthState, RootState> = {
 
   getters: {
     getUserInfo(state) {
-      return state.userInfo;
+      return Object.keys(state.userInfo).length
+        ? state.userInfo
+        : {
+            id: 1,
+            name: "Leanne Graham",
+            username: "Bret",
+            email: "Sincere@april.biz",
+            address: {
+              street: "Kulas Light",
+              suite: "Apt. 556",
+              city: "Gwenborough",
+              zipcode: "92998-3874",
+              geo: {
+                lat: "-37.3159",
+                lng: "81.1496",
+              },
+            },
+            phone: "1-770-736-8031 x56442",
+            website: "hildegard.org",
+            company: {
+              name: "Romaguera-Crona",
+              catchPhrase: "Multi-layered client-server neural-net",
+              bs: "harness real-time e-markets",
+            },
+          };
     },
     getLoginError(state) {
       return state.loginError;
