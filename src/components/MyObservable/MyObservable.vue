ac9510bc3aa33d95f467abd94df3133619f0ad34
mykhail.svekolnykov <Mykhail.Svekolnykov.-ND@disney.com>
2023-10-23 16:41:11 +0300

    Tried observable pattern

---
+++ src/components/MyObservable/MyObservable.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup="">
+import observable from "./ClassObservable";
+function handleClick() {
+  observable.notify("User clicked button!");
+}
+
+function logger(data) {
+  console.log(`${Date.now()} ${data}`);
+}
+
+function logger2(data) {
+  console.log(`loger 2`);
+}
+
+function logger3(data) {
+  console.log(`loger 3`);
+}
+
+observable.subscribe(logger);
+observable.subscribe(logger2);
+observable.subscribe(logger3);
+</script>
+
+<template>
+  <div>
+    <button v-on:click="handleClick">Click to notify observables</button>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,16 +1,19 @@
 <script lang="ts">
 import Vue from "vue";
 import LoginForm from "../components/LoginForm.vue";
+import MyObservable from "../components/MyObservable/MyObservable.vue";
 
 export default Vue.extend({
   components: {
     LoginForm,
+    MyObservable,
   },
 });
 </script>
 
 <template>
   <div class="container">
+    <MyObservable />
     <LoginForm />
   </div>
 </template>
